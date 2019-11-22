eeee078e8cab34b92f47f04353d961f56fe5a29a
Bhanu Pratap <pratap.bhanu3434@gmail.com>
2019-11-22 21:30:17 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+    <strong> &copy; 2019 Brought you by <a href="https://github.com/ibhanu/">Bhanu Pratap and Aadithyaa Rajath</a></strong>
+</footer>
